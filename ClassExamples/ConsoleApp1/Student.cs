2f2e90e40ce32a8d8737e36a2034910259b60e31
Lakin Cromar <Lakin.Cromar@ou.edu>
2020-01-23 13:09:57 -0600

    new example

---
+++ ClassExamples/ConsoleApp1/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        private int LuckyNumber;
+
+        public Student()
+        {
+
+        }
+        public string BirthdateDayOfWeek()
+        {
+            return;
+        }
+    }
+}
