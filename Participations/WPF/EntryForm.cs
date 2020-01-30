435320c288f5608cf8a6fe786e3771e6690d76de
Lakin Cromar <Lakin.Cromar@ou.edu>
2020-01-30 12:47:38 -0600

    have a great day katie

---
+++ Participations/WPF/EntryForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+        public EntryForm()
+        {
+            Name = "";
+            Address = "";
+            ZipCode = 0;
+        }
+
+        public EntryForm(string name, string address, int zipCode)
+        {
+            Name = "";
+            Address = "";
+            ZipCode = 0;
+        }
+        
+        public override string ToString()
+        {
+            string sentence = ($"{Name} lives at {Address} with a zipcode of {ZipCode}");
+            return sentence;
+        }
+    }
+}
---
+++ Participations/WPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            EntryForm entry = new EntryForm();
+            entry.Name = TBName.Text;
+            entry.Address = TBAddress.Text;
+            entry.ZipCode = Convert.ToInt32(TBZipcode.Text);
+            LB.Items.Add(entry.ToString());
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+          
+        }
+        
+    }
+}
