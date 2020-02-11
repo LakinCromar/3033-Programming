c9326ba63870b7be61239c76b351c61f1c492e9f
Lakin Cromar <Lakin.Cromar@ou.edu>
2020-02-11 13:14:41 -0600

    new example

---
+++ ClassExamples/WPF_JSON/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
+using System.Net.Http;
+using System.Windows;
+using Newtonsoft.Json;
+
+namespace WPF_JSON
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string apiURL = "https://rickandmortyapi.com/api/character/";
+            RickMortyAPI apiInfo;
+            using (var client = new HttpClient()) 
+            {
+                string json = client.GetStringAsync(apiURL).Result;
+
+                apiInfo = JsonConvert.DeserializeObject<RickMortyAPI>(json);
+            }
+            foreach (var charater in apiInfo.results)
+            {
+                lsbchar.Items.Add(charater);
+            }
+            
+
+        }
+
+        private void lsbchar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ResultObject selectedchar = (ResultObject)lsbchar.SelectedItem;
+            //selectedchar.image;
+            imgchar.Source = new BitmapImage(new System.Uri(selectedchar.image));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ResultObject selectedchar = (ResultObject)lsbchar.SelectedItem;
+            //().source = new 
+        }
+    }
+}
---
+++ ClassExamples/WPF_JSON/RickMortyAPI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_JSON
+{
+    class RickMortyAPI
+    {
+        //2 properties 
+        public InfoObject info { get; set; }
+        public List<ResultObject> results { get; set; }
+
+    }
+    public class InfoObject
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public string prev { get; set; }
+
+    }
+
+    public class ResultObject
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+        public string staus { get; set; }
+        public string species { get; set; }
+        public string gender { get; set; }
+        public string image { get; set; }
+        public string url { get; set;  }
+    public override string ToString()
+    {
+        return $"({id}) {name}";
+    }
+    }
+    
+}
