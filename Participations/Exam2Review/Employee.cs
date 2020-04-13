ad86bf5e5edbfe2b94b2aa75aeefcfdd542446d3
Lakin Cromar <Lakin.Cromar@ou.edu>
2020-04-13 14:27:29 -0500

    new

---
+++ Participations/Exam2Review/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace EmployeeExample
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+        public Employee()
+        {
+            FirstName = "";
+            LastName = "";
+            Email = "";
+            Gender = "";
+            Salary = 0;
+        }
+
+    }
+    public override ToString()
+    {
+
+    }
+
+}
---
+++ Participations/Exam2Review/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Exam2Review
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string path = @"C:\Users\Lakin\Downloads\Employees.txt";
+            string lines = File.ReadAllText(path);
+            string[] pieces = lines.Split('|');
+            foreach (var piece in pieces)
+            {
+                Employee emp = new Employee();
+                emp.FirstName = pieces[5];
+                emp.LastName = pieces[6];
+                emp.Email = pieces[7];
+                emp.Gender = pieces[8];
+                //emp.Salary = Convert.ToDouble(pieces[9].Remove(0, 1));
+
+
+            }
+            //string path = @"C:\Users\Lakin\Downloads\Employees.txt";
+            //string lines = File.ReadAllText(path);
+            //string [] pieces = lines.Split('|');
+            foreach (var piece in pieces)
+            {
+                if (piece.Contains("@dropbox.com"))
+                {
+                    lstEmail.Items.Add(piece);
+                }
+
+            }
+
+            avgSalary.Text = "80,000";
+
+        }
+
+        private void lstSalary_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void lstEmail_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void avgSalary_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
